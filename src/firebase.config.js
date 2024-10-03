76676bd5aa931375f9b65917188873ac8cd7a874
KushagraSharma1004 <161684316+KushagraSharma1004@users.noreply.github.com>
2024-10-03 11:57:14 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyCRK2izk5LvUkWh5aq-m-300AtEZZpElEI",
+  authDomain: "eekhgames.firebaseapp.com",
+  projectId: "eekhgames",
+  storageBucket: "eekhgames.appspot.com",
+  messagingSenderId: "1046928875257",
+  appId: "1:1046928875257:web:0a1f2a00f09541bb514c28",
+  measurementId: "G-YK4V9GVGTJ"
 };
 
 // Initialize Firebase
